b71013ea3972e108c0503351978760ddab13fb6d
Jan Hovland <jan.hovland@lyse.net>
2023-11-06 19:51:47 +0100

    Update

--- WeatherForecast/Views/Moon/MoonView.swift
+++ WeatherForecast/Views/Moon/MoonView.swift
@@ -25,6 +25,9 @@ struct MoonView: View {
             }
             .opacity(0.50)
             .padding(.top, -5)
+            ///
+            /// Viser måne fasen
+            ///
             ZStack {
                 HStack {
                     Spacer()
@@ -36,18 +39,76 @@ struct MoonView: View {
             .opacity(0.50)
             .offset(x: UIDevice.isIpad ? 85 : 85,
                     y: UIDevice.isIpad ? -20 : -20)
-            
-//            .opacity(0.50)
-//            .padding(.leading, -40)
-            
-//            Text(currentWeather.moonPhase)
-            Text(FindTimeFromAmPm(time: currentWeather.moonrise))
-            Text(FindTimeFromAmPm(time: currentWeather.moonset))
-            Text("\(currentWeather.moonIllumination)")
-            Text("Moon: \(currentWeather.isMoonUp)")
-            Text("Sun: \(currentWeather.isSunUp)")
-
-            
+            ///
+            /// Visernstyrken på lyset fra månen:
+            ///
+            HStack {
+                HStack {
+                    Text("Illumination")
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text("\(currentWeather.moonIllumination) %")
+                }
+            }
+            .padding(.bottom, 5)
+            ///
+            /// Månenedgang:
+            ///
+            HStack {
+                HStack {
+                   Text("MoonSet")
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text(FindTimeFromAmPm(time: currentWeather.moonset))
+                }
+            }
+            .padding(.bottom, 5)
+            ///
+            /// Måneoppgang:
+            ///
+            HStack {
+                HStack {
+                   Text("MoonRise")
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text(FindTimeFromAmPm(time: currentWeather.moonrise))
+                }
+            }
+            .padding(.bottom, 5)
+            ///
+            /// Er månen synlig:
+            ///
+            HStack {
+                HStack {
+                   Text("Is the moon visible?")
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text(currentWeather.isMoonUp == 1 ? "Yes" : "No")
+                }
+            }
+            .padding(.bottom, 5)
+            ///
+            /// Er solen synlig:
+            ///
+            HStack {
+                HStack {
+                   Text("Is the sun visible?")
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Text(currentWeather.isSunUp == 1 ? "Yes" : "No")
+                }
+            }
+            .padding(.bottom, 5)
             Spacer()
         }
         .frame(width: UIDevice.isIpad ? 358 : 358,
