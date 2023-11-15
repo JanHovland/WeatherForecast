2854df85aef81d6b47c5c6b9363a3ca272e582d6
Jan Hovland <jan.hovland@lyse.net>
2023-11-15 21:55:16 +0100

    Updaste

--- WeatherForecast/Utilities/FindMoonPhaseImage.swift
+++ WeatherForecast/Utilities/FindMoonPhaseImage.swift
@@ -18,20 +18,25 @@ func FindMoonPhaseImage(moonPhase: String,
            moonIllumination < 1 {
             image = "New Moon"
         } else if moonIllumination >= 1,
-           moonIllumination < 5 {
-            image = "Waxing Crescent 5"
+                  moonIllumination < 5 {
+            image = "Waxing Crescent 1"
         } else if moonIllumination >= 5,
-           moonIllumination <  11 {
+                  moonIllumination <  11 {
             image = "Waxing Crescent 2"
         } else if moonIllumination >= 11,
-            moonIllumination < 19 {
+                  moonIllumination < 19 {
             image = "Waxing Crescent 3"
         } else if moonIllumination >= 19,
-            moonIllumination < 28 {
+                  moonIllumination < 28 {
             image = "Waxing Crescent 4"
         } else if moonIllumination >= 28,
-            moonIllumination < 38 {
+                  moonIllumination < 38 {
             image = "Waxing Crescent 5"
+        }
+    } else if moonPhase == String(localized: "First Quarter") {
+        if moonIllumination > 38 ,
+           moonIllumination <= 50 {
+            image = "First Quarter"
         }
     }
     
