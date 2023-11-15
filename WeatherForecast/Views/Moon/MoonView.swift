f2999962829721db29ecd796b95e52e4f9c948c2
Jan Hovland <jan.hovland@lyse.net>
2023-11-15 20:38:04 +0100

    Update

--- WeatherForecast/Views/Moon/MoonView.swift
+++ WeatherForecast/Views/Moon/MoonView.swift
@@ -12,36 +12,43 @@ struct MoonView: View {
     
     var body: some View {
         VStack {
-            ///
-            /// Viser overskriften for fluftkvaliteten:
-            ///
             HStack {
-                Image(systemName: "moon")
-                    .renderingMode(.original)
-                    .font(Font.headline.weight(.regular))
-                Text("MOON")
-                    .font(.system(size: 15, weight: .bold))
-                Spacer()
-            }
-            .opacity(0.50)
-            .padding(.top, -5)
-            ///
-            /// Viser måne fasen
-            ///
-            ZStack {
+                ///
+                /// Viser overskriften for fluftkvaliteten:
+                ///
+                HStack {
+                    Image(systemName: "moon")
+                        .renderingMode(.original)
+                        .font(Font.headline.weight(.regular))
+                    Text("MOON")
+                        .font(.system(size: 15, weight: .bold))
+                    Spacer()
+                }
+                ///
+                /// Viser måne fasen
+                ///
                 HStack {
                     Spacer()
                     Text(currentWeather.moonPhase.uppercased())
                         .font(.system(size: 15, weight: .bold))
-                    Spacer()
                 }
-                .padding(.leading, 25)
             }
             .opacity(0.50)
-            .offset(x: UIDevice.isIpad ? 85 : 85,
-                    y: UIDevice.isIpad ? -20 : -20)
             ///
-            /// Visernstyrken på lyset fra månen:
+            /// Viser selve månen:
+            ///
+            Image("moonVoksende månesigd 6")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .foregroundColor(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 90)
+                        .foregroundColor(.black.opacity(0.20))
+                    
+                )
+            ///
+            /// Viser styrken på lyset fra månen:
             ///
             HStack {
                 HStack {
@@ -53,7 +60,6 @@ struct MoonView: View {
                     Text("\(currentWeather.moonIllumination) %")
                 }
             }
-            .padding(.bottom, 5)
             ///
             /// Månenedgang:
             ///
@@ -67,7 +73,6 @@ struct MoonView: View {
                     Text(FindTimeFromAmPm(time: currentWeather.moonset))
                 }
             }
-            .padding(.bottom, 5)
             ///
             /// Måneoppgang:
             ///
@@ -81,35 +86,6 @@ struct MoonView: View {
                     Text(FindTimeFromAmPm(time: currentWeather.moonrise))
                 }
             }
-            .padding(.bottom, 5)
-            ///
-            /// Er månen synlig:
-            ///
-            HStack {
-                HStack {
-                   Text("Is the moon visible?")
-                    Spacer()
-                }
-                HStack {
-                    Spacer()
-                    Text(currentWeather.isMoonUp == 1 ? "Yes" : "No")
-                }
-            }
-            .padding(.bottom, 5)
-            ///
-            /// Er solen synlig:
-            ///
-            HStack {
-                HStack {
-                   Text("Is the sun visible?")
-                    Spacer()
-                }
-                HStack {
-                    Spacer()
-                    Text(currentWeather.isSunUp == 1 ? "Yes" : "No")
-                }
-            }
-            .padding(.bottom, 5)
             Spacer()
         }
         .frame(width: UIDevice.isIpad ? 358 : 358,
