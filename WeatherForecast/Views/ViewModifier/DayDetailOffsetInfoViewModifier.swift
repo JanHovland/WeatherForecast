0eaae75455ef6178060bb27924a09ac9642b4d8f
Jan Hovland <jan.hovland@lyse.net>
2023-11-28 08:31:45 +0100

    Updatr

--- WeatherForecast/Views/ViewModifier/DayDetailOffsetInfoViewModifier.swift
+++ WeatherForecast/Views/ViewModifier/DayDetailOffsetInfoViewModifier.swift
@@ -20,19 +20,19 @@ struct DayDetailOffsetInfoViewModifier: ViewModifier {
             content.offset(y: UIDevice.isIpad ? -320 : -260)
             
         } else if self.option == .wind {
-            content.offset(y: UIDevice.isIpad ? -290 : -245)
+            content.offset(y: UIDevice.isIpad ? -290 : -210)
             
         } else if self.option == .precipitation {
-            content.offset(y: UIDevice.isIpad ? -320 : -255)
+            content.offset(y: UIDevice.isIpad ? -320 : -225)
             
         } else if self.option == .feelsLike {
-            content.offset(y: UIDevice.isIpad ? -295 : -275)
+            content.offset(y: UIDevice.isIpad ? -295 : -235)
             
         } else if self.option == .humidity {
-            content.offset(y: UIDevice.isIpad ? -300 : -225)
+            content.offset(y: UIDevice.isIpad ? -300 : -210)
             
         } else if self.option == .visibility {
-            content.offset(y: UIDevice.isIpad ? -300 : -245)
+            content.offset(y: UIDevice.isIpad ? -300 : -190)
             
         } else if self.option == .airPressure {
             content.offset(y: UIDevice.isIpad ? -290 : -200)
