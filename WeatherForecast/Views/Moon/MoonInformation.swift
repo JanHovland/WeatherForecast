0f75edabdd4ef94ae247f31639aea4bae35ffae2
Jan Hovland <jan.hovland@lyse.net>
2025-09-29 11:34:46 +0200

    Commit

--- WeatherForecast/Views/Moon/MoonInformation.swift
+++ WeatherForecast/Views/Moon/MoonInformation.swift
@@ -1,9 +1,9 @@
-//
-//  MoonInformation.swift
-//  WeatherForecast
-//
-//  Created by Jan Hovland on 29/09/2025.
-//
+    //
+    //  MoonInformation.swift
+    //  WeatherForecast
+    //
+    //  Created by Jan Hovland on 29/09/2025.
+    //
 
 import SwiftUI
 
@@ -15,12 +15,12 @@ struct MoonInformation: View {
         VStack {
             
             ZStack {
-//                HStack {
-//                    Image(systemName: image)
-//                        .symbolRenderingMode(.multicolor)
-//                        .font(Font.headline.weight(.regular))
-//                    Spacer()
-//                }
+                    //                HStack {
+                    //                    Image(systemName: image)
+                    //                        .symbolRenderingMode(.multicolor)
+                    //                        .font(Font.headline.weight(.regular))
+                    //                    Spacer()
+                    //                }
                 HStack {
                     Spacer()
                     VStack {
@@ -32,7 +32,7 @@ struct MoonInformation: View {
                                 .font(.system(size: 22, weight: .bold))
                                 .foregroundColor(.red)
                                 .padding(.trailing, 20)
-                       })
+                        })
                     }
                     .padding(.top, 20)
                 }
@@ -42,22 +42,49 @@ struct MoonInformation: View {
                 .font(.system(size: 130))
             
             Text(String(format: NSLocalizedString(currentWeather.moonPhase, comment: "")))
+            
+            VStack {
+                HStack(spacing: UIDevice.isIpad ? 20 : 0) {
+                    HStack {
+                        Spacer()
+                        Text("Illumination").textCase(.uppercase)
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text("MoonRise").textCase(.uppercase)
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text("MoonSet").textCase(.uppercase)
+                        Spacer()
+                    }
+                }
+                .font(.caption)
                 
-            
-            
-            
-                // Color.green.opacity(0.3).ignoresSafeArea()
-//            Color("Background#01")
-//                .opacity(0.35)
-//                .ignoresSafeArea()
-            
-                 
-            
+                HStack(spacing: 20) {
+                    HStack {
+                        Spacer()
+                        Text(currentWeather.moonIllumination)
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text(currentWeather.moonrise)
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text(currentWeather.moonset)
+                        Spacer()
+                    }
+                }
+            }
+            .padding(.horizontal, UIDevice.isIpad ? 80 : 0)
+            .padding(.top,10)
             Spacer()
-                 
-                
-            
         }
-        // .modifier(DayDetailBackground(dayLight: currentWeather.isDaylight))
-     }
+    }
 }
+
